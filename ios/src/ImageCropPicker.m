5f6c16e21c0b0f8a356b65e29d9b25e8ef4c8db4
nadav <nadav2051@gmail.com>
2019-07-10 14:41:50 +0300

    Update ImageCropPicker.m

--- ios/src/ImageCropPicker.m
+++ ios/src/ImageCropPicker.m
@@ -372,7 +372,17 @@ RCT_EXPORT_METHOD(openPicker:(NSDictionary *)options
                     imagePickerController.mediaType = QBImagePickerMediaTypeAny;
                 }
             }
-
+            
+            NSString *color = [self.options objectForKey:@"tintColor"];
+            if (color) {
+                unsigned rgbValue = 0;
+                NSScanner *scanner = [NSScanner scannerWithString:color];
+                [scanner setScanLocation:1]; // bypass '#' character
+                [scanner scanHexInt:&rgbValue];
+                UIColor *tintColor = [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+                [[imagePickerController view] setTintColor:tintColor];
+            }
+            
             [[self getRootVC] presentViewController:imagePickerController animated:YES completion:nil];
         });
     }];
